1eca8920991055118000076186bdc25027866fe9
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-02-02 16:21:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-// Complete the js code
 function Car(make, model) {
-	this.make=make;
-	this.model=model
-}
-Car.prototype.getMakeModel=function(){
-	return  this.make  +  this. model 
+    this.make = make;
+    this.model = model;
 }
 
+// Adding method to Car prototype
+Car.prototype.getMakeModel = function() {
+    return `{this.make} {this.model}`
+};
+
+// Constructor function for SportsCar, inheriting from Car
 function SportsCar(make, model, topSpeed) {
-	this.make=make;
-	this.model=model;
-	this.topSpeed=topSpeed;
-}
-SportsCar.prototype.getTopSpeed=function(){
-	return this.topSpeed.
+    // Use the Car constructor within SportsCar
+    Car.call(this, make, model);
+    
+    this.topSpeed = topSpeed;
 }
 
-Car.protopype__proto__=object.create(SportsCar.prototype )
+// Inheriting Car prototype into SportsCar prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Adding method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
+
+// Correcting typo in setting prototype
+SportsCar.prototype.constructor = SportsCar;
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
