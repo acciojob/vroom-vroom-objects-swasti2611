36260047a822fe1865905793a7af4dc228a4449f
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-02-02 16:06:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model
+}
+Car.prototype.getMakeModel=function(){
+	return  this.make  +  this. model 
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	this.make=make;
+	this.model=model;
+	this.topSpeed=topSpeed;
+}
+SportsCar.prototype.getTopSpeed=function(){
+	return this.topSpeed.
+}
 
+Car.protopype__proto__=object.create(SportsCar.prototype )
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
